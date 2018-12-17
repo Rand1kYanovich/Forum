4293e804be86bf592c6c8e1b13baa2b045d24660
Daniil <rand1kch@gmail.com>
2018-12-17 20:01:14 +0400

    Start release third fragment(Notifications)

---
+++ app/src/main/java/com/grafixartist/bottomnav/InfoFragment.java
@@ -0,0 +1,53 @@
+package com.grafixartist.bottomnav;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+public class InfoFragment extends Fragment {
+    DataAdapter adapter;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_COLOR = "color";
+
+    // TODO: Rename and change types of parameters
+    private int color;
+
+
+    public InfoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            color = getArguments().getInt(ARG_COLOR);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.info_fragment, container, false);
+        rootView.findViewById(R.id.meme);
+        return rootView;
+    }
+
+
+
+
+
+
+}
